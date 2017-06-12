82f8b1c3a806f6e036ba22a531cafa080a184e28
TYLER LANGENBRUNNER <MISTERLANGO@GMAIL.COM>
2017-06-12 15:56:00 -0700

    Added seed for both dev and test

--- routes/books.js
+++ routes/books.js
@@ -1,10 +1,16 @@
 'use strict';
 
 const express = require('express');
-
+const knex = require('../db/knex.js');
 // eslint-disable-next-line new-cap
 const router = express.Router();
 
-// YOUR CODE HERE
+router.get('/books', (req, res) => {
+  knex('books')
+    .select('*')
+    .then((books) => {
+      res.status(200).json(books);
+    });
+});
 
 module.exports = router;
