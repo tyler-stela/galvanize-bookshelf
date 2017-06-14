34e3c11b4ac4d119cff95c4388b28046071d495b
TYLER LANGENBRUNNER <MISTERLANGO@GMAIL.COM>
2017-06-14 11:37:33 -0700

    Added post method to books

--- routes/books.js
+++ routes/books.js
@@ -51,28 +51,27 @@ router.get('/books/:id', (req, res) => {
 });
 
 router.post('/books', (req, res) => {
-  console.log(req.body);
-  console.log('Hello!!!!')
-  //knex.schema.raw("SELECT setval(‘books_id_seq’, (SELECT MAX(id) FROM books))")
-
   knex('books')
-    // .select('id')
-    // .(max('id'))
-    .insert(humps.decamelizeKeys(req.body), '*')
-    // .insert({
-    //   title: req.body.title,
-    //   author: req.body.author,
-    //   genre: req.body.genre,
-    //   description: req.body.description,
-    //   cover_url: req.body.coverUrl
-    // }, '*')
+    .insert({
+      title: req.body.title,
+      author: req.body.author,
+      genre: req.body.genre,
+      description: req.body.description,
+      cover_url: req.body.coverUrl
+    }, '*')
     .then((books) => {
-      console.log(books);
       if (!books) {
         res.sendStatus(404);
       }
-      //res.set('Content-Type', 'application/json');
-      res.status(200).json(books);
+      const book = books[0];
+      res.json({
+        id: book.id,
+        coverUrl: book.cover_url,
+        author: book.author,
+        description: book.description,
+        title: book.title,
+        genre: book.genre
+      })
     })
     .catch(err =>
       {console.log('errrrrrr', err)
