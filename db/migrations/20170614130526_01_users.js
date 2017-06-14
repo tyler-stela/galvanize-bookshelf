0c8fb03775ff4a11f08b982f842ffd43b377a1ec
TYLER LANGENBRUNNER <MISTERLANGO@GMAIL.COM>
2017-06-14 13:30:05 -0700

    Added users migration, seed, and also seeded new table called users

---
+++ db/migrations/20170614130526_01_users.js
@@ -0,0 +1,17 @@
+'use strict';
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('users', (table) => {
+    table.increments('id').primary();
+    table.string('first_name').notNullable().defaultTo('');
+    table.string('last_name').notNullable().defaultTo('');
+    table.string('email').notNullable().unique();
+    table.specificType('hashed_password', 'char(60)').notNullable();
+    table.timestamp('created_at').notNullable().defaultTo(knex.fn.now());
+    table.timestamp('updated_at').notNullable().defaultTo(knex.fn.now());
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('users');
+};
