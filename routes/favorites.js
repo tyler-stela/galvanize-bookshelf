549a2bc94c3c15c95a27f8aafa41a2ce8f8667db
TYLER LANGENBRUNNER <MISTERLANGO@GMAIL.COM>
2017-06-16 15:44:41 -0700

    Fixed set header error for favorites routes

--- routes/favorites.js
+++ routes/favorites.js
@@ -6,17 +6,20 @@
 // knex migrate:latest
 
 const bodyParser = require('body-parser');
+const cookieParser = require( 'cookie-parser' )
 const express = require('express');
 const humps = require('humps');
 const knex = require('../knex.js');
 
 // eslint-disable-next-line new-cap
+const app = express();
 const router = express.Router();
-
+app.use( cookieParser() );
 
 router.get('/favorites', (req, res) => {
   if ( !req.cookies.token ) {
     res.status( 401 ).set('Content-Type', 'text/plain').send( 'Unauthorized' );
+    return;
   }
   knex('favorites')
     .select('*')
@@ -25,12 +28,13 @@ router.get('/favorites', (req, res) => {
       let result = humps.camelizeKeys(favorites)
       res.status(200).json(result)
     })
-    .catch(err => res.sendStatus(404))
+    // .catch(err => res.sendStatus(404))
 });
 
 router.get('/favorites/:id', (req, res) => {
   if ( !req.cookies.token ) {
     res.status( 401 ).set('Content-Type', 'text/plain').send( 'Unauthorized' );
+    return;
   }
   knex('favorites')
     .select('*')
@@ -40,37 +44,34 @@ router.get('/favorites/:id', (req, res) => {
     .then((favoriteBook) => {
       if (!favoriteBook) {
         res.status(200).send(false)
+        return;
       }
       res.status(200).send(true)
     })
-    .catch(err => {
-      res.sendStatus(404)
-    })
+    // .catch(err => res.sendStatus(404))
 });
 
 router.post('/favorites', (req, res) => {
   if ( !req.cookies.token ) {
     res.status( 401 ).set('Content-Type', 'text/plain').send( 'Unauthorized' );
+    return;
   }
   knex('favorites')
     .insert({book_id: req.body.bookId, user_id: 1}, '*')
     .into('favorites')
     .then((newFavorite) => {
-      // console.log('new favorite?', newFavorite[0]);
       if (!newFavorite[0]) {
         res.sendStatus(401);
       }
       res.status(200).json(humps.camelizeKeys(newFavorite[0]));
     })
-    .catch(err => {
-      // console.log('catch all', err);
-      res.sendStatus(404)
-    })
+    // .catch(err => res.sendStatus(404))
 })
 
 router.delete('/favorites', (req, res, next) => {
   if ( !req.cookies.token ) {
     res.status( 401 ).set('Content-Type', 'text/plain').send( 'Unauthorized' );
+    return;
   }
   let deletedFavorite;
   knex('favorites')
@@ -89,39 +90,7 @@ router.delete('/favorites', (req, res, next) => {
       delete deletedFavorite[0].id
       res.status(200).send(humps.camelizeKeys(deletedFavorite[0]));
     })
-    .catch(err => {
-      console.log('catch all', err);
-      res.sendStatus(404)
-    })
-
+    // .catch(err => res.sendStatus(404))
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 module.exports = router;
