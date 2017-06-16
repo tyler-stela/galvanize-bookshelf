65bc032db652c8dc9acd0723fe89e185a0f29f86
TYLER LANGENBRUNNER <MISTERLANGO@GMAIL.COM>
2017-06-16 14:48:07 -0700

    Added first two get methods for favorites route

--- routes/favorites.js
+++ routes/favorites.js
@@ -5,11 +5,43 @@
 // knex seed:run
 // knex migrate:latest
 
+const bodyParser = require('body-parser');
 const express = require('express');
+const humps = require('humps');
+const knex = require('../knex.js');
 
 // eslint-disable-next-line new-cap
 const router = express.Router();
 
-// YOUR CODE HERE
+
+router.get('/favorites', (req, res) => {
+  knex('favorites')
+    .select('*')
+    .innerJoin('books', 'favorites.id', 'books.id' )
+    .then((favorites) => {
+      let result = humps.camelizeKeys(favorites)
+      res.status(200).json(result)
+    })
+    .catch(err => res.sendStatus(404))
+});
+
+router.get('/favorites/:id', (req, res) => {
+  knex('favorites')
+    .select('*')
+    .innerJoin('books', 'favorites.id', 'books.id' )
+    .where('book_id', req.query.bookId)
+    .first()
+    .then((favorite_book) => {
+      if (!favorite_book) {
+        res.status(200).send(false)
+      }
+      res.status(200).send(true)
+    })
+    .catch(err => {
+      res.sendStatus(404)
+    })
+});
+
+
 
 module.exports = router;
