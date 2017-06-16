4f49b6b5e7295fa92784a1784743f9683033e331
TYLER LANGENBRUNNER <MISTERLANGO@GMAIL.COM>
2017-06-15 19:46:51 -0700

    Started working on migration and seeding--stuck on foreign keys error

--- routes/favorites.js
+++ routes/favorites.js
@@ -1,5 +1,10 @@
 'use strict';
 
+// knex migrate:make 01_favorites
+// knex seed:make favorites
+// knex seed:run
+// knex migrate:latest
+
 const express = require('express');
 
 // eslint-disable-next-line new-cap
