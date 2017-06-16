e64e43d75227dd6a65af2a86045e1449c541a009
TYLER LANGENBRUNNER <MISTERLANGO@GMAIL.COM>
2017-06-16 16:40:53 -0700

    Added heroku database url to knexfile

--- knexfile.js
+++ knexfile.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const DATABASE_URL = 'postgres://whvpmhxyseuosf:cc6af349e5a4e4ffa1580a987fddf1791ee261ef9ea119bc24bcecb5e31217bd@ec2-54-235-120-39.compute-1.amazonaws.com:5432/dcai5jf3s4vinj'
+
 module.exports = {
   development: {
     client: 'pg',
@@ -23,7 +25,7 @@ module.exports = {
   },
   production: {
     client: 'pg',
-    connection: 'postgres://localhost/bookshelf_production',
+    connection: `postgres://${DATABASE_URL}/bookshelf_production`,
     migrations: {
       directory: `${__dirname}/db/migrations`
     },
